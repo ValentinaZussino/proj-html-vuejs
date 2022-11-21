90a92e31ea2ddf0b5990613d6492529d4e02d89a
ValentinaZussino <vzussino@protonmail.com>
2022-11-21 12:10:24 +0100

    agg colori e componenti principali

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
-  <div>
-    ciao funziono
-  </div>
+  <HeaderComponent/>
+  <main>main main main</main>
+  <FooterComponent/>
 </template>
 
 <script>
+import HeaderComponent from './components/HeaderComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
+
   export default {
-    
-  }
+    components: { HeaderComponent, FooterComponent }
+}
 </script>
 
 <style lang="scss" scoped>
