0052e09bbdafb67758993a94aadde15f1526e468
ValentinaZussino <vzussino@protonmail.com>
2022-11-22 15:25:34 +0100

    sistemaz logos section

--- src/components/BrandsSection.vue
+++ src/components/BrandsSection.vue
@@ -1,6 +1,15 @@
 <template>
     <section>
-        brands section
+        <div class="vz_container">
+            <span>brand logos</span>
+            <div class="logos-container">
+                <div class="single-logo"><img src="/img/b_logotype_6.png" alt="" class="h-100"></div>
+                <div class="single-logo"><img src="/img/b_logotype_7.png" alt="" class="h-100"></div>
+                <div class="single-logo"><img src="/img/b_logotype_1.png" alt="" class="h-100"></div>
+                <div class="single-logo"><img src="/img/b_logotype_2.png" alt="" class="h-100"></div>
+                <div class="single-logo"><img src="/img/b_logotype_3.png" alt="" class="h-100"></div>
+            </div>
+        </div>
     </section>
 </template>
 
@@ -16,7 +25,32 @@
 
 section {
     width: 100%;
-    height: 190px;
+    height: 210px;
     background-color: $softgray;
+
+    div.vz_container {
+        height: 100%;
+        margin: 0 auto;
+        text-align: center;
+        padding: 40px 0;
+
+        span{
+            text-transform: uppercase;
+            font-size: 11px;
+            color: $textgraycolor;
+            letter-spacing: 1.2px;
+        }
+        .logos-container{
+            width: 100%;
+            height: 100px;
+            margin-top: 15px;
+            @include dflex-base;
+
+            .single-logo{
+                height: 100%;
+                width: calc(100% / 5);
+            }
+        }
+    }
 }
 </style>
